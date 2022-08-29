c34837cacac5adb18d9406d09718adb71cbae11a
Wendeley <wendeleyf@gmail.com>
2022-08-29 16:58:27 -0300

    Add raridade e restilização da tabela

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -10,7 +10,15 @@ import { Constants } from 'src/app/config/constants';
 export class CardListComponent implements OnInit {
   
   cardList:any = [];
-  displayedColumns: string[] = ['id', 'name', 'status', 'rarity', 'source', 'patch'];
+  rarities = {
+    rarity1 : 'assets/img/rarity1.png',
+    rarity2 : 'assets/img/rarity2.png',
+    rarity3 : 'assets/img/rarity3.png',
+    rarity4 : 'assets/img/rarity4.png',
+    rarity5 : 'assets/img/rarity5.png',
+  }
+
+  displayedColumns: string[] = ['id', 'icon','rarity', 'name', 'status', 'source', 'patch'];
 
   constructor(private http: HttpClient) { }
   
